ff0671c7040f2ea20cb8253b2eabfb46c25e94f1
Rax <34583543+MrRax786@users.noreply.github.com>
2024-01-31 21:37:45 +0000

    changes made

--- 26020-lab3-s-haskell_p31278rh-main/submission.hs
+++ 26020-lab3-s-haskell_p31278rh-main/submission.hs
@@ -1,6 +1,7 @@
 
 -- Quadtree Definition
-data Quadtree = Cell Bool | Node Quadtree Quadtree Quadtree Quadtree
+data Quadtree = Cell Bool 
+    | Node Quadtree Quadtree Quadtree Quadtree
     deriving (Eq, Show)
 
 -- Function for Black Cell creation
@@ -21,4 +22,39 @@ anticlockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
 anticlockwise a b c d = Node d c b a
 
 
+
+
+
+
+
+
+-- data Quadtree = Cell Bool 
+--     | Node Quadtree Quadtree Quadtree Quadtree
+
+-- instance Eq Quadtree where 
+--     (Cell c1) == (Cell c2) = c1 == c2
+--     (Node q1 q2 q3 q4) == (Node q1' q2' q3' q4') = q1 == q1' && q2 == q2' && q3 == q3' && q4 == q4'
+--     _ ==_= False
+
+-- instance Show Quadtree where
+--     show(Cell True) = " "
+--     show (Cell False) = " "
+--     show(Node q1 q2 q3 q4) = "/n" ++ show q1 ++ show q2 ++ show q3 ++ show q4
+
+-- -- Function for Black Cell creation
+-- allBlack :: Int -> Quadtree
+-- allBlack _ = Cell True
+
+-- -- Function for White Cell creation
+-- allWhite :: Int -> Quadtree
+-- allWhite _ = Cell False
+
+-- -- Clockwise Function
+-- clockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
+-- clockwise q1 q2 q3 q4 = Node q1 q2 q3 q4
+
+
+-- -- Anticlockwise Function
+-- anticlockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
+-- anticlockwise q1 q2 q3 q4 = Node q1 q2 q3 q4
 
