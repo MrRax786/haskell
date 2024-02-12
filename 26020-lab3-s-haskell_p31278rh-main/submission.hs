2711c3aa04aa22de67911a16632b819ca1a47487
Rax <34583543+MrRax786@users.noreply.github.com>
2024-02-12 17:03:31 +0000

    Submission UPDATED

--- 26020-lab3-s-haskell_p31278rh-main/submission.hs
+++ 26020-lab3-s-haskell_p31278rh-main/submission.hs
@@ -1,3 +1,6 @@
+import System.Console.Terminfo (Color(White))
+import Distribution.Compat.Graph (neighbors)
+import GHC.Base (neChar, neAddr#)
 ----------------------- Ex1 --------------------------
 
 -- Quadtree Definition
@@ -28,6 +31,118 @@ anticlockwise a b c d = Node a d c b
 
 -- ----------------------- Ex2 --------------------------
 
+
+
+-- Get Colour of Cell
+--getColour :: Bool -> [Bool] -> Bool
+--getColour isBlack neighbours =
+    --let neighbourTOT = length neighbours
+        --oppColourCount = occuranceCount (not isBlack) neighbours
+   -- in if oppColourCount > (quot neighbourTOT 2)
+        --then not isBlack
+       -- else isBlack
+
+-- -- Blur Function
 -- blur :: Quadtree -> Quadtree
--- blur 0 = 0
--- blur n = 
+-- blur BlackCell = WhiteCell
+-- blur WhiteCell = BlackCell
+-- blur(Node a b c d) =
+--     let neighbours = map blur [a,b,c,d]
+--         blurredColour = getColour True neighbours
+--     in if blurredColour
+--         then BlackCell
+--         else WhiteCell
+
+--------------------------------------------------------------------------
+
+-- Get Cell Colour
+getCellColour :: Quadtree -> Bool
+getCellColour WhiteCell = False
+getCellColour BlackCell = True
+
+-- Check if node is cell
+isCell:: Quadtree -> Bool
+isCell quadtree = case quadtree of
+    WhiteCell _ -> True
+    BlackCell _ -> True
+    _ -> False
+
+-- Count Func for Num Opposite Colour Neighbours
+oppColourCount :: Bool -> [Quadtree] -> Int
+oppColourCount colour neighbors = length $ filter (\x -> x /= colour) (map getCellColour neighbors)
+
+-- Get blurred cell colour
+blurredColourd :: Quadtree -> [Quadtree] -> Bool
+blurredColour quadtree neighbours = let
+    totNeighbours = length Neighbours
+    oppColourCount' = oppColourCount (not $ getCellColour quadtree ) neighbour
+    in if oppColourCount' > totNeighbours 'div' 2
+        then not (getCellColour quadtree)
+        else getCellColour quadtree
+
+-- Blur Single Cell Function 
+cellBlur :: Quadtree -> [Quadtree] -> Quadtree
+cellBlur quadtree neighbors = if isCell quadtree
+    then if oppColourCount (getCellColour quadtree) neighbors > length neighbors 'div' 2
+        in if getCellColour quadtree then BlackCell else WhiteCell
+    else quadtree
+    else quadtree
+
+-- Blur Quadtree
+blur :: Quadtree -> Quadtree
+blur quadtree = let
+    nodeBlur :: Quadtree -> Quadtree
+    nodeBlur (Node a b c d) = Node (blurNode a) (blurNode b) (blurNode c) (blurNode d)
+    nodeBlur node = node
+
+    cellsBlur :: Quadtree -> [Quadtree] -> Quadtree
+    cellsBlur quadtree neighbors = if isCell quadtree
+        then cellBlur quadtree Neighbours
+        else quadtree
+
+    treeBlurCells :: Quadtre -> Quadtree
+    treeBlurCells (Node a b c d) = Node (treeBlurCells a) (treeBlurCells b) (treeBlurCells c) (treeBlurCells d)
+    treeBlurCells node = node
+
+    in treeBlurCells $ blurNode quadtree 
+
+
+-- -- Single Cell Blur
+-- cellBlur :: Quadtree -> [Bool] -> Quadtree
+-- cellBlur (Cell isBlack) neighbours = Cell (getColour isBlack neighbours)
+-- cellBlur q _ = q
+
+-- -- Entire Quadtree Blur
+-- blur :: Quadtree -> Quadtree
+-- blur q = mapQuadtreeWNeighbours cellBlur q
+
+-- -- Map Function
+-- mapQuadtreeWNeighbours :: ([Bool] -> Quadtree -> Quadtree) -> Quadtree -> Quadtree
+-- mapQuadtreeWNeighbours f q = mapQuadtreeWNeighbours' f q []
+
+-- -- Helper Function
+-- mapQuadtreeWNeighbours' :: ([Bool] -> Quadtree -> Quadtree) -> Quadtree -> [Bool] -> Quadtree
+-- mapQuadtreeWNeighbours' _ (Cell _)_ = Cell False -- BC
+-- mapQuadtreeWNeighbours' f (Node a b c d) neighbours =
+--     let newNeighbours = getNeighbours (map getCellColour [a b c d])
+--         newA = mapQuadtreeWNeighbours' f a newNeighbours
+--         newB = mapQuadtreeWNeighbours' f b newNeighbours
+--         newC = mapQuadtreeWNeighbours' f c newNeighbours
+--         newD = mapQuadtreeWNeighbours' f d newNeighbours
+--     in f neighbours (Node newA newB newC newD)
+
+
+
+
+
+
+
+-- -- Neighbour Colours Function
+-- getNeighbours :: [Bool] -> Bool
+-- getNeighbours [a, b, c, d] = [a, b, c, d]
+
+-- -- Occurance Count
+-- occuranceCount :: Eq a => a -> [a] -> Int
+-- occuranceCount x = length . filter (== x)
+
+
